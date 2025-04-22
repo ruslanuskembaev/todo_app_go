710c4baccd771915829d8d8718dd86d7cc76def2
Руслан Ускембаев <ruslanuskembaev02@gmail.com>
2025-04-23 01:29:29 +0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module todo_app_go
+
+go 1.24.2
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	// "todo_app_go/handlers"
+	// "todo_app_go/models"
+)
+
+func main() {
+	fmt.Println("todo_app")
+
+}
